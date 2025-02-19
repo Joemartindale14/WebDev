0ad22ab0631fcf57ab229954f6fc2304e8f5e6b9
Joemartindale14 <mar23006620@heartofyorkshire.ac.uk>
2025-02-19 15:50:33 +0000

    Updated Version 2.4
    Made sure the correct version of vite is installed.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Ensure the port is set to 5173
+  },
 });
