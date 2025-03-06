22c412df3154f74ae2038a0b1b21717faad150d7
Joemartindale14 <mar23006620@heartofyorkshire.ac.uk>
2025-03-06 21:33:44 +0000

    Update SignIn.jsx

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -9,7 +9,7 @@ const [password, setPassword] = useState("");
 const handleSubmit = async (e) => {
   e.preventDefault();
   try {
-    const response = await axios.post(`http://localhost:5000/api/auth/signin`, {
+    const response = await axios.post(`/api/auth/signin`, {
       email,
       password,
     });
