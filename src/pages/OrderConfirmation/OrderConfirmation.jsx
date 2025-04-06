2d0bc8c6dded2412c891bf5cf060779e1b778d9b
Joemartindale14 <mar23006620@heartofyorkshire.ac.uk>
2025-04-06 22:29:21 +0100

    Updated Version 06/04/25

--- src/pages/OrderConfirmation/OrderConfirmation.jsx
+++ src/pages/OrderConfirmation/OrderConfirmation.jsx
@@ -4,7 +4,7 @@ import "./OrderConfirmation.css";
 
 const OrderConfirmation = () => {
   const navigate = useNavigate();
-  const [countdown, setCountdown] = useState(5); // Countdown starts at 5 seconds
+  const [countdown, setCountdown] = useState(5);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -16,8 +16,8 @@ const OrderConfirmation = () => {
     }, 5000);
 
     return () => {
-      clearInterval(timer); // Clear interval to avoid memory leaks
-      clearTimeout(redirectTimer); // Clear timeout if component unmounts
+      clearInterval(timer);
+      clearTimeout(redirectTimer);
     };
   }, [navigate]);
 
@@ -28,7 +28,7 @@ const OrderConfirmation = () => {
         <p>
           Your order has been successfully placed.<br />You can now collect your items from the gym reception.<br />If there are any issues, please contact us.
         </p>
-        <p>You will be directed to the homepage in {countdown} seconds...</p>
+        <p>You will be directed to the homepage in {countdown} seconds.</p>
       </div>
       <a href="/Merchandise">
         <button>Continue Shopping</button>
